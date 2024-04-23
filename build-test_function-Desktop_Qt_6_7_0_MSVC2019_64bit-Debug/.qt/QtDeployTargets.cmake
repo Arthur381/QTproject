6d94b0e850f91908126e6c462d3dbf6172ccf526
Arthur381 <17709377230@163.com>
2024-04-23 11:05:10 +0800

    test_pull_function

---
+++ build-test_function-Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_test_function_FILE D:/Courses/The second semester/QT/Myproject/build-test_function-Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/test_function.exe)
+set(__QT_DEPLOY_TARGET_test_function_RUNTIME_DLLS F:/project/QT/6.7.0/msvc2019_64/bin/Qt6Widgetsd.dll;F:/project/QT/6.7.0/msvc2019_64/bin/Qt6Guid.dll;F:/project/QT/6.7.0/msvc2019_64/bin/Qt6Cored.dll)
