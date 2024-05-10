d84658f50955327a8ce066d2773f120ea3dcf6f2
msy79lucky <msy77999@gmail.com>
2024-05-10 16:26:39 +0800

    加了sqlite库

--- realProgect/dailymsy_allin.cpp
+++ realProgect/dailymsy_allin.cpp
@@ -18,3 +18,9 @@ void dailymsy_allin::on_missionAdd_clicked()//添加任务并且显示
 
 }
 
+void dailymsy_allin::CreatDataFunc(){//创建SQLite数据库
+
+    }
+void dailymsy_allin::CreatTableFunc(){//创建sqlite数据表
+
+    }
--- realProgect/dailymsy_allin.h
+++ realProgect/dailymsy_allin.h
@@ -3,6 +3,12 @@
 
 #include <QDialog>
 
+#include <QSqlDatabase>//用于连接，创建数据库
+#include <QSqlError>
+#include <QSqlQuery>//专用于DML（数据操纵语言），DDL（数据定义语言）
+#include <QSqlQueryModel>
+#include <QtDebug>
+
 namespace Ui {
 class dailymsy_allin;
 }
@@ -20,6 +26,13 @@ private slots:
 
 private:
     void CreatDataFunc();//创建SQLite数据库
+    void CreatTableFunc();//创建sqlite数据表
+
+    QSqlDatabase sqldb;//创建qt和数据库连接
+    QSqlQueryModel sqimodel;//存储结果集
+
+
+private:
     Ui::dailymsy_allin *ui;
 };
 
