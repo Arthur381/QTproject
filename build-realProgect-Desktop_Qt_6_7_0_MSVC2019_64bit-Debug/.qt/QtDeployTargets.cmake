6d93abba09c068006c2dd70af2ad17edae5c18f3
Arthur381 <17709377230@163.com>
2024-05-10 11:31:14 +0800

    本地删除所有build文件

--- build-realProgect-Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_realProgect_FILE D:/Courses/The second semester/QT/Myproject/build-realProgect-Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/realProgect.exe)
-set(__QT_DEPLOY_TARGET_realProgect_RUNTIME_DLLS F:/project/QT/6.7.0/msvc2019_64/bin/Qt6Widgetsd.dll;F:/project/QT/6.7.0/msvc2019_64/bin/Qt6Guid.dll;F:/project/QT/6.7.0/msvc2019_64/bin/Qt6Cored.dll)
