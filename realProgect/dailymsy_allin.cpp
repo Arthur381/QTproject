9cc3ece00d414f25cb2dbe0a4063ad412f24d415
msy79lucky <msy77999@gmail.com>
2024-05-13 19:55:42 +0800

    创建了msy的数据表

--- realProgect/dailymsy_allin.cpp
+++ realProgect/dailymsy_allin.cpp
@@ -1,6 +1,9 @@
 #include "dailymsy_allin.h"
 #include "ui_dailymsy_allin.h"
 #include <QMessageBox>
+
+#include<QDebug>
+dailymsy_allin* dailymsy_allin::ptrdailymsy_allin = nullptr;//在类外初始化
 dailymsy_allin::dailymsy_allin(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::dailymsy_allin)
@@ -38,15 +41,33 @@ void dailymsy_allin::CreatDataFunc(){//创建SQLite数据库
     else{
         QMessageBox::critical(0,"错误","数据库打开失败",QMessageBox::Ok);
     }
+}
+
+QList<BEventInfo> dailymsy_allin::getPage(int page,int uicnt){//根本目的是得到列表
+    QList<BEventInfo> l;
+    QSqlQuery sql(sqldb);
+    QString strsql=QString("select * from event order by id limit %1 offset %2")
+                         .arg(uicnt).arg(page*uicnt);
+    sql.exec(strsql);
+    int id;
+    BEventInfo info;
+    while(sql.next()){
+        id=sql.value(0).toInt();
+        info.thingsname=sql.value(1).toString();
+        info.im=sql.value(2).toString();
+        info.em=sql.value(3).toString();
+        l.push_back(info);
     }
+    return l;
+}
 void dailymsy_allin::CreatTableFunc(){//创建sqlite数据表
     QSqlQuery creatquery;
 
-    QString strsql="creat table ThingsDemo ("
-                             "DescribeThings text primary key autoincrement,"
-                             "importantNum integerr not null,"
-                             "emergentNum integer not null)";
-
+    QString strsql=QString("create table event("
+                                    "id int not null,"
+                                    "thingsname text primary key not null,"
+                                    "im int not null,"
+                                    "em int not null)");
 
     //执行SQL语句
     if(creatquery.exec(strsql)==false){
--- realProgect/dailymsy_allin.h
+++ realProgect/dailymsy_allin.h
@@ -2,7 +2,7 @@
 #define DAILYMSY_ALLIN_H
 
 #include <QDialog>
-
+#include<QList>
 #include <QSqlDatabase>//用于连接，创建数据库
 #include <QSqlError>
 #include <QSqlQuery>//专用于DML（数据操纵语言），DDL（数据定义语言）
@@ -13,13 +13,31 @@ namespace Ui {
 class dailymsy_allin;
 }
 
+struct BEventInfo
+{
+    QString id;
+    QString thingsname;
+    QString im;
+    QString em;
+};
+
 class dailymsy_allin : public QDialog
 {
     Q_OBJECT
 
 public:
     explicit dailymsy_allin(QWidget *parent = nullptr);
+    static dailymsy_allin *ptrdailymsy_allin;//类内声明的静态指针
+
+    static dailymsy_allin *getinstance(){//单例化
+        if(nullptr==ptrdailymsy_allin){
+            ptrdailymsy_allin=new dailymsy_allin;
+        }
+        return ptrdailymsy_allin;
+    }
+    QList<BEventInfo> getPage(int page,int uicnt);//从数据库中读取列表
     ~dailymsy_allin();
+
 
 private slots:
     void on_missionAdd_clicked();
