164f1419e0278b7760f18b36b0c87256352006b9
Arthur381 <17709377230@163.com>
2024-05-28 11:24:03 +0800

    主窗口

--- realProgect/mainwindow.cpp
+++ realProgect/mainwindow.cpp
@@ -22,30 +22,100 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->calBT->setText("日历");
-    ui->calBT->setStyleSheet(
+    /**
+     *
+     *
+     *配色的第二部分
+     *
+     *
+     */
+    ui->readme->setStyleSheet(
         "QPushButton {"
-        "    background-color: lightgray;"
+        "    background-color: rgba(247, 22, 60, 0.865);"
         "    border: 2px solid rgba(115, 177, 166, 0.865);"//设置边框
         "    color:rgb(5, 12, 12);"// 设置按钮文本的颜色为黑色（RGB值为5, 12, 12）
         "    border-radius: 6;"// 设置按钮的边框半径为6像素，使其圆角化
         "    font: 10pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
         "}"
         "QPushButton:hover {"
-        "    background-color: lightyellow;"
+        "    background-color: rgba(251, 205, 205, 0.84);"
         "    font: 18pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
         "    transition: all 0.1s ease;"
         "}"
         );
-    ui->style->setStyleSheet(        "QPushButton {"
-        "    background-color: lightgray;"
-
+    ui->TTBT->setStyleSheet(
+        "QPushButton {"
+        "    background-color: rgba(247, 22, 60, 0.865);"
         "    border: 2px solid rgba(115, 177, 166, 0.865);"//设置边框
         "    color:rgb(5, 12, 12);"// 设置按钮文本的颜色为黑色（RGB值为5, 12, 12）
         "    border-radius: 6;"// 设置按钮的边框半径为6像素，使其圆角化
         "    font: 10pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
         "}"
         "QPushButton:hover {"
-        "    background-color: lightyellow;"
+        "    background-color: rgba(251, 205, 205, 0.84);"
+        "    font: 18pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "    transition: all 0.1s ease;"
+        "}"
+        );
+    ui->clockBT->setStyleSheet(
+        "QPushButton {"
+        "    background-color: rgba(247, 22, 60, 0.865);"
+        "    border: 2px solid rgba(115, 177, 166, 0.865);"//设置边框
+        "    color:rgb(5, 12, 12);"// 设置按钮文本的颜色为黑色（RGB值为5, 12, 12）
+        "    border-radius: 6;"// 设置按钮的边框半径为6像素，使其圆角化
+        "    font: 10pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "}"
+        "QPushButton:hover {"
+        "    background-color: rgba(251, 205, 205, 0.84);"
+        "    font: 18pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "    transition: all 0.1s ease;"
+        "}"
+        );
+    /**
+     *
+     *
+     *配色的第二部分
+     *
+     *
+     */
+    ui->calBT->setStyleSheet(
+        "QPushButton {"
+        "    background-color: rgba(239, 135, 135, 0.717);"
+        "    border: 2px solid rgba(115, 177, 166, 0.865);"//设置边框
+        "    color:rgb(5, 12, 12);"// 设置按钮文本的颜色为黑色（RGB值为5, 12, 12）
+        "    border-radius: 6;"// 设置按钮的边框半径为6像素，使其圆角化
+        "    font: 10pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "}"
+        "QPushButton:hover {"
+        "    background-color: rgba(244, 54, 54, 0.84);"
+        "    font: 18pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "    transition: all 0.1s ease;"
+        "}"
+        );
+    ui->pushButton->setStyleSheet(
+        "QPushButton {"
+        "    background-color: rgba(239, 135, 135, 0.717);"
+        "    border: 2px solid rgba(115, 177, 166, 0.865);"//设置边框
+        "    color:rgb(5, 12, 12);"// 设置按钮文本的颜色为黑色（RGB值为5, 12, 12）
+        "    border-radius: 6;"// 设置按钮的边框半径为6像素，使其圆角化
+        "    font: 10pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "}"
+        "QPushButton:hover {"
+        "    background-color: rgba(244, 54, 54, 0.84);"
+        "    font: 18pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "    transition: all 0.1s ease;"
+        "}"
+        );
+    ui->style->setStyleSheet(
+        "QPushButton {"
+        "    background-color: rgba(239, 135, 135, 0.717);"
+        "    border: 2px solid rgba(115, 177, 166, 0.865);"//设置边框
+        "    color:rgb(5, 12, 12);"// 设置按钮文本的颜色为黑色（RGB值为5, 12, 12）
+        "    border-radius: 6;"// 设置按钮的边框半径为6像素，使其圆角化
+        "    font: 10pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
+        "}"
+        "QPushButton:hover {"
+        "    background-color: rgba(244, 54, 54, 0.84);"
         "    font: 18pt '楷体';"// 设置按钮文本的字体为 10 点大小的楷体
         "    transition: all 0.1s ease;"
         "}"
